64c6b36f47d7a19188e61bad49948bcc753f919b
offa <bm-dev@yandex.com>
2017-11-22 21:58:43 +0100

    Don't push images on Travis CI.

--- script/build-image.sh
+++ script/build-image.sh
@@ -8,5 +8,7 @@ IMG_PATH=$2
 DEV_IMAGE=${DEPLOY_REGISTRY}/${IMG}
 
 docker build --pull -t ${DEV_IMAGE} ${IMG_PATH}
-docker push ${DEV_IMAGE}
 
+if [ "$3" != "--no-push" ]; then
+    docker push ${DEV_IMAGE}
+fi
