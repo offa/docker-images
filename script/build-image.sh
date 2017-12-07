1b0614a5bb2c25c84e88c37abb8738fff4496e40
offa <bm-dev@yandex.com>
2017-12-07 20:23:27 +0100

    Travis CI build updated. Images aren't released on those.

--- script/build-image.sh
+++ script/build-image.sh
@@ -5,7 +5,6 @@
 #   1: Image Name
 #   2: Path to Dockerfile
 #   3: Version [Optional]
-#   4: --no-push - Only build the image [Optional]
 
 
 set -ex
@@ -17,12 +16,14 @@ OPT_ARG=$4
 
 DEV_IMAGE=${DEPLOY_REGISTRY}/${IMG}
 
-if [ ! -z "${VERSION}" ]; then
+if [ ! -z "${VERSION}" ]
+then
     BUILD_ARG="--build-arg VERSION=${VERSION}"
 fi
 
 docker build --pull ${BUILD_ARG} -t ${DEV_IMAGE} ${IMG_PATH}
 
-if [ "${OPT_ARG}" != "--no-push" ]; then
+if [ ! -z "${IMAGE_PUSH}" ]
+then
     docker push ${DEV_IMAGE}
 fi
