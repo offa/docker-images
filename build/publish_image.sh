f1759bc29c1befa80d8bb3d3e46bc483fb208ef3
offa <bm-dev@yandex.com>
2023-05-09 18:19:44 +0200

    Update build and deployment (#129)

---
+++ build/publish_image.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DEPLOY_BRANCH="master"
+
+if [[ "${GIT_COMMIT_BRANCH}" != "${DEPLOY_BRANCH}" && "${CI_COMMIT_REF_NAME}" != "${DEPLOY_BRANCH}" ]]; then
+    echo "Skip deployment"
+    exit 0
+fi
+
+if [[ -z "${DEPLOY_REGISTRY}" ]]; then
+    echo "No DEPLOY_REGISTRY set"
+    exit 1
+fi
+
+docker push ${DEPLOY_REGISTRY}/${1}
