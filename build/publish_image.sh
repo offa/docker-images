2ab3df29afbe109d54495fb114081d627290f032
offa <bm-dev@yandex.com>
2023-05-10 18:35:57 +0200

    Use sh instead of bash

--- build/publish_image.sh
+++ build/publish_image.sh
@@ -1,15 +1,15 @@
-#!/bin/bash
+#!/bin/sh
 
 DEPLOY_BRANCH="master"
 
-if [[ "${GIT_COMMIT_BRANCH}" != "${DEPLOY_BRANCH}" && "${CI_COMMIT_REF_NAME}" != "${DEPLOY_BRANCH}" ]]; then
+if [ "${GIT_COMMIT_BRANCH}" != "${DEPLOY_BRANCH}" ] && [ "${CI_COMMIT_REF_NAME}" != "${DEPLOY_BRANCH}" ]; then
     echo "Skip deployment"
     exit 0
 fi
 
-if [[ -z "${DEPLOY_REGISTRY}" ]]; then
+if [ -z "${DEPLOY_REGISTRY}" ]; then
     echo "No DEPLOY_REGISTRY set"
     exit 1
 fi
 
-docker push ${DEPLOY_REGISTRY}/${1}
+docker push "${DEPLOY_REGISTRY}/${1}"
