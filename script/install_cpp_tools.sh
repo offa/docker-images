05192720ae3f54d5ca3faf6dd0a2c67ff4024380
offa <8887756+offa@users.noreply.github.com>
2025-09-01 17:44:17 +0200

    Update CMake v4.1.1

--- script/install_cpp_tools.sh
+++ script/install_cpp_tools.sh
@@ -5,7 +5,7 @@ set -ex
 apt-get update
 apt-get install -y --no-install-recommends build-essential
 
-CMAKE_VERSION="4.1.0"
+CMAKE_VERSION="4.1.1"
 
 curl -sSL "https://github.com/Kitware/CMake/releases/download/v${CMAKE_VERSION}/cmake-${CMAKE_VERSION}-linux-x86_64.sh" -o install-cmake.sh
 chmod +x install-cmake.sh
